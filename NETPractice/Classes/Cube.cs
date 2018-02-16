5dcfa86416b211f6956b84c345dba026f4987c4a
ongoni <ongoni.fg@gmail.com>
2018-02-16 21:11:00 +0400

    add cube validation

--- NETPractice/Classes/Cube.cs
+++ NETPractice/Classes/Cube.cs
@@ -7,19 +7,19 @@ namespace NETPractice.Classes {
 
     public class Cube {
 
-        private readonly List<Point> vertices = new List<Point>();
-        public readonly double length;
+        private readonly List<Point> _vertices = new List<Point>();
+        public readonly double Length;
 
         public Cube() {
-            vertices.Add(new Point(0, 0, 0));
-            vertices.Add(new Point(1, 0, 0));
-            vertices.Add(new Point(1, 1, 0));
-            vertices.Add(new Point(0, 1, 0));
-            vertices.Add(new Point(0, 0, 1));
-            vertices.Add(new Point(1, 0, 1));
-            vertices.Add(new Point(1, 1, 1));
-            vertices.Add(new Point(0, 1, 1));
-            length = 1;
+            _vertices.Add(new Point(0, 0, 0));
+            _vertices.Add(new Point(1, 0, 0));
+            _vertices.Add(new Point(1, 1, 0));
+            _vertices.Add(new Point(0, 1, 0));
+            _vertices.Add(new Point(0, 0, 1));
+            _vertices.Add(new Point(1, 0, 1));
+            _vertices.Add(new Point(1, 1, 1));
+            _vertices.Add(new Point(0, 1, 1));
+            Length = 1;
         }
 
         public Cube(string path) {
@@ -29,50 +29,77 @@ namespace NETPractice.Classes {
                     string[] coords = line.Split(' ');
                     if (coords.Length != 3) throw new InvalidDataException();
 
-                    try {
-                        vertices.Add(
-                            new Point(
-                                Double.Parse(coords[0]), 
-                                Double.Parse(coords[1]), 
-                                Double.Parse(coords[2])
-                            )
-                        );
-                    } catch (Exception e) {
-                        Console.WriteLine(e.Message);
-                    }
+                    _vertices.Add(
+                        new Point(
+                            double.Parse(coords[0]),
+                            double.Parse(coords[1]),
+                            double.Parse(coords[2])
+                        )
+                    );
                     
                     line = streamReader.ReadLine();
                 }
+                
+                if (_vertices.Count != 8) throw new InvalidDataException();
             }
 
-            length = GetLength();
-            if (!isValid()) throw new InvalidDataException();
+            Length = GetLength();
+            if (!IsValid()) throw new InvalidDataException();
         }
         
+        // минимальное расстояние от текущей точки до всех остальных считаем длиной ребра куба
         private double GetLength() {
-            Point temp = vertices.First();
-            List<double> distances = new List<double>();
-            List<Point> filteredVertices = vertices.Where(x => x != temp).ToList();
-
-            foreach (Point vertex in filteredVertices) {
-                distances.Add(Point.GetDistance(temp, vertex));
-            }
-
-            return distances.Min();
+            Point temp = _vertices.First();
+            
+            return _vertices
+                .Where(x => x != temp)
+                .Select(vertex => Point.GetDistance(temp, vertex))
+                .Min();
         }
         
-        private bool isValid() {
-            //quick maths
-            // ...
-            return true;
+        // если существует 
+        // а) ровно три вершины, удалённых от текущей на длину ребра куба
+        // б) ровно три вершины, удалённых от текущей на длину диагонали грани куба
+        // в) ровно одна вершина, удалённая от текущей на длину диагонали куба
+        // то данные 8 точек считаем кубом
+        public bool IsValid() {
+            double cubeFaceDiagonal = Math.Sqrt(2) * Length;    // диагональ грани куба
+            double cubeDiagonal = Math.Sqrt(Math.Pow(Length, 2) + Math.Pow(cubeFaceDiagonal, 2));    // диагональ куба
+            bool valid = false;
+
+            int cubeEdgeCounter = 0,
+                cubeFaceDiagonalCounter = 0,
+                cubeDiagonalCounter = 0;
+            
+            for (int i = 0; i < _vertices.Count; i++) {
+                for (int j = 1; j < _vertices.Count; j++) {
+                    if (Math.Abs(Point.GetDistance(_vertices[i], _vertices[j]) - Length) < 0.001) {
+                        cubeEdgeCounter++;
+                        continue;
+                    }
+
+                    if (Math.Abs(Point.GetDistance(_vertices[i], _vertices[j]) - cubeFaceDiagonal) < 0.001) {
+                        cubeFaceDiagonalCounter++;
+                        continue;
+                    }
+
+                    if (Math.Abs(Point.GetDistance(_vertices[i], _vertices[j]) - cubeDiagonal) < 0.001) {
+                        cubeDiagonalCounter++;
+                    }
+                }
+
+                if (cubeEdgeCounter == 3 && cubeFaceDiagonalCounter == 3 && cubeDiagonalCounter == 1) valid = true;
+            }
+
+            return valid;
         }
         
         public double GetArea() {
-            return length * length;
+            return Length * Length;
         }
 
         public double GetCubage() {
-            return GetArea() * length;
+            return GetArea() * Length;
         }
         
     }
--- NETPractice/Program.cs
+++ NETPractice/Program.cs
@@ -6,7 +6,12 @@ namespace NETPractice {
     internal class Program {
 
         public static void Main(string[] args) {
+            Cube test = new Cube();
             Cube cube = new Cube("../../cubeExample.txt");
+            Console.WriteLine(cube.GetArea());
+            Console.WriteLine(cube.GetCubage());
+            Console.WriteLine(test.IsValid());
+            Console.WriteLine(cube.IsValid());
         }
 
     }
