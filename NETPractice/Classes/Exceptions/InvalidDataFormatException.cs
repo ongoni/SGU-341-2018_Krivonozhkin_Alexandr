075e479baa6944fa1f9627c51aa38d99dce8f32c
ongoni <ongoni.fg@gmail.com>
2018-02-17 11:21:10 +0400

    add InvalidDataFormatException

---
+++ NETPractice/Classes/Exceptions/InvalidDataFormatException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NETPractice.Classes.Exceptions {
+
+    public class InvalidDataFormatException: Exception {
+
+        public InvalidDataFormatException() { }
+
+        public InvalidDataFormatException(string message) : base(message) { }
+        
+    }
+
+}
