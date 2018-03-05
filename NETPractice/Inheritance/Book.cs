b4cc12b187bb33319f451be454d5d5f0996bc67c
ongoni <ongoni.fg@gmail.com>
2018-03-05 15:18:50 +0400

    add inheritance task

---
+++ NETPractice/Inheritance/Book.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+
+namespace NETPractice.Inheritance
+{
+    public class Book
+    {
+        private string _name;
+        private int _pageCount;
+        private string _publisher;
+        private DateTime _publishingDate;
+        private DateTime _writingDate;
+        private Author _author;
+
+        public Book()
+        {
+            _author = new Author();
+        }
+
+        public Book(string name, int pageCount, string publisher, DateTime publishingDate, 
+            DateTime writingDate, Author author)
+        {
+            Name = name;
+            PageCount = pageCount;
+            Publisher = publisher;
+            PublishingDate = publishingDate;
+            WritingDate = writingDate;
+            Author = author;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new InvalidDataException("empty string given");
+                }
+
+                _name = value;
+            }
+        }
+
+        public int PageCount
+        {
+            get => _pageCount;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new InvalidDataException("page count can't be negative or 0");
+                }
+
+                _pageCount = value;
+            }
+        }
+
+        public string Publisher
+        {
+            get => _publisher;
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new InvalidDataException("empty string given");
+                }
+
+                _publisher = value;
+            }
+        }
+
+        public DateTime PublishingDate
+        {
+            get => _publishingDate;
+            set
+            {
+                if (value.Date > Author.BirthDate.Date || value.Date > WritingDate.Date)
+                {
+                    throw new InvalidDataException("publishing date can't be before the author birth date " +
+                                                   "or writing date");
+                }
+
+                _publishingDate = value;
+            }
+        }
+
+        public DateTime WritingDate
+        {
+            get => _writingDate;
+            set
+            {
+                if (value.Date > Author.BirthDate.Date)
+                {
+                    throw new InvalidDataException("writing date can't be before the author birth date");
+                }
+
+                _writingDate = value;
+            }
+        }
+
+        public Author Author
+        {
+            get => _author;
+            set
+            {
+                if (value == null)
+                {
+                    throw new InvalidDataException("author can't be null");
+                }
+
+                _author = value;
+            }
+        }
+
+    }
+}
---
+++ NETPractice/Inheritance/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace NETPractice.Inheritance
+{
+    public sealed class Student : Man
+    {
+        private DateTime _educationStartDate;
+        private int _courseNumber;
+        private string _groupNumber;
+
+        public Student() { }
+
+        public Student(string name, int age, double weight, double height, DateTime birthDate) 
+            : base(name, age, weight, height, birthDate)
+        { }
+        
+        public Student(string name, int age, double weight, double height, DateTime birthDate, 
+            DateTime educationStartDate, int courseNumber, string groupNumber) 
+            : base(name, age, weight, height, birthDate)
+        {
+            EducationStartDate = educationStartDate;
+            CourseNumber = courseNumber;
+            GroupNumber = groupNumber;
+        }
+
+        public DateTime EducationStartDate
+        {
+            get => _educationStartDate;
+            set
+            {
+                if (value.Date >= BirthDate.Date)
+                {
+                    throw new InvalidDataException("education start date can't be before the birth date");
+                }
+
+                _educationStartDate = value;
+            }
+        }
+
+        public int CourseNumber
+        {
+            get => _courseNumber;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new InvalidDataException("course number can't be negative or 0");
+                }
+
+                _courseNumber = value;
+            }
+        }
+
+        public string GroupNumber
+        {
+            get => _groupNumber;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new InvalidDataException("empty string given");
+                }
+
+                _groupNumber = value;
+            }
+        }
+
+    }
+    
+}
